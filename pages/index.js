9cc922b12f8fa95580251dc180d8f8c3e65a5f29
qqyule <qqyule@gmail.com>
2020-08-07 21:23:36 +0800

    gaga

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome tooooo <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
